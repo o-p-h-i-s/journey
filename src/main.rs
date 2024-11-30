61d861d3e140447636ed5e1f57cec9e0978947bf
o-p-h-i-s <80509766+o-p-h-i-s@users.noreply.github.com>
2024-12-01 02:59:25 +0900

    feat: 空のエディタ

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{widget::text_editor, Element, Length};
+
+fn main() -> iced::Result {
+    iced::application("journey", Editor::update, Editor::view).run()
+}
+
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action),
+}
+
+struct Editor {
+    contents: text_editor::Content,
+}
+
+impl Default for Editor {
+    fn default() -> Self {
+        Self {
+            contents: text_editor::Content::new(),
+        }
+    }
+}
+
+impl Editor {
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::Edit(action) => self.contents.perform(action),
+        }
+    }
+
+    fn view(&self) -> Element<Message> {
+        text_editor(&self.contents)
+            .on_action(Message::Edit)
+            .height(Length::Fill)
+            .into()
+    }
 }
