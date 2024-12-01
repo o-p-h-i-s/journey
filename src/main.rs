e3da47594c7277237cf03aaec3b3868262e56819
o-p-h-i-s <80509766+o-p-h-i-s@users.noreply.github.com>
2024-12-01 23:28:57 +0900

    feat: openボタンの有効化

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-use std::path::PathBuf;
+use std::{fs, path::PathBuf};
 
 use iced::{
+    futures::io,
     widget::{button, column, container, horizontal_space, row, text_editor},
     Element, Length,
 };
@@ -33,7 +34,12 @@ impl Editor {
     fn update(&mut self, message: Message) {
         match message {
             Message::Edit(action) => self.contents.perform(action),
-            Message::Open => {}
+            Message::Open => {
+                if let Ok((path, contents)) = open_file() {
+                    self.path = Some(path);
+                    self.contents = text_editor::Content::with_text(&contents);
+                }
+            }
         }
     }
 
@@ -52,4 +58,31 @@ impl Editor {
 
         container(main).padding(5).into()
     }
+}
+
+#[derive(Debug)]
+enum Error {
+    FailedOpenFile,
+    FailedLoadFile(io::Error),
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Error::FailedOpenFile => write!(f, "Failed to open file"),
+            Error::FailedLoadFile(err) => write!(f, "Failed to load file: {:?}", err),
+        }
+    }
+}
+
+impl std::error::Error for Error {}
+
+fn open_file() -> Result<(PathBuf, String), Error> {
+    let path = rfd::FileDialog::new()
+        .set_directory("./")
+        .pick_file()
+        .ok_or(Error::FailedOpenFile)?;
+    let contents = fs::read_to_string(&path).map_err(|err| Error::FailedLoadFile(err))?;
+
+    Ok((path, contents))
 }
