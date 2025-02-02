944027c83bc67bcb7281af8f6bcf5d3fd95de446
o-p-h-i-s <80509766+o-p-h-i-s@users.noreply.github.com>
2025-02-02 13:50:13 +0900

    reset

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-mod buffer;
-mod pane;
-
-use iced::window::Settings;
-use iced::{keyboard, Element, Subscription, Task};
+use iced::{widget::horizontal_space, window::Settings, Element, Subscription, Task};
 
 fn main() -> iced::Result {
     iced::application(Journey::title, Journey::update, Journey::view)
@@ -13,59 +9,29 @@ fn main() -> iced::Result {
         .run_with(Journey::new)
 }
 
-#[derive(Debug)]
-struct Journey {
-    pane: pane::Pane,
-}
+struct Journey {}
 
 #[derive(Debug, Clone)]
-enum Message {
-    Pane(pane::Message),
-}
+enum Message {}
 
 impl Journey {
     fn new() -> (Self, Task<Message>) {
-        (
-            Self {
-                pane: pane::Pane::new(),
-            },
-            Task::none(),
-        )
+        (Self {}, Task::none())
     }
 
     fn title(&self) -> String {
         "Journey".to_string()
     }
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::Pane(message) => {
-                self.pane.update(message);
-            }
-        }
+    fn update(&mut self, message: Message) -> Task<Message> {
+        match message {}
     }
 
     fn view(&self) -> Element<Message> {
-        self.pane.view().map(Message::Pane)
+        horizontal_space().into()
     }
 
     fn subscription(&self) -> Subscription<Message> {
-        keyboard::on_key_press(|key, modifiers| keybinding(key, modifiers))
-    }
-}
-
-fn keybinding(key: keyboard::Key, modifiers: keyboard::Modifiers) -> Option<Message> {
-    use iced::widget::pane_grid::Axis;
-    use keyboard::Key;
-
-    if !modifiers.control() {
-        return None;
-    }
-
-    match key.as_ref() {
-        Key::Character("v") => Some(Message::Pane(pane::Message::SplitFocused(Axis::Vertical))),
-        Key::Character("h") => Some(Message::Pane(pane::Message::SplitFocused(Axis::Horizontal))),
-        Key::Character("c") => Some(Message::Pane(pane::Message::CloseFocused)),
-        _ => None,
+        Subscription::none()
     }
 }
